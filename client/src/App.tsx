480e8f33a33c606934ccd3c1d8813ef666868d2d
Abner do Nascimento Santos <abnerdonantos06@gmail.com>
2024-08-22 00:47:15 -0300

    chore: add firebase dependency and configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
-import { Backpack } from 'lucide-react'
 import './App.css'
-import TourCard from './components/TourCard/TourCard'
+
 
 function App() {
 
   return (
     <>
-      <TourCard 
-        icon={<Backpack size={36} />}
-        type='Adventure'
-        quantity='10 Tours+'
-        price={250}
-      />
+        
     </>
   )
 }
