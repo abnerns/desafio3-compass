b13e88d155193e2a12ecf01355e3ab1d011eb6fd
Abner do Nascimento Santos <abnerdonantos06@gmail.com>
2024-08-22 17:13:51 -0300

    feat: add bootstrap and toastify dependencies for styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Router from './routes/Router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
