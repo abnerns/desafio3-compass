3c45441b34eafb6c8dfc09aef3d11251e42777e9
Abner do Nascimento Santos <abnerdonantos06@gmail.com>
2024-08-23 23:07:33 -0300

    style: update home body to implement components

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,14 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import styles from "./Home.module.css"
 
 function Home() {
   
 
   return (
-    <div>
+    <div className={styles.body}>
       <Header />
+      <Footer />
     </div>
   );
 }
