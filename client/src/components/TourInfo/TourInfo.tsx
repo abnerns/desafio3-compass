a74b35a4c23d87428af212f03f8196c5f4cb637e
Abner do Nascimento Santos <abnerdonantos06@gmail.com>
2024-08-27 18:02:21 -0300

    feat: add tour info components and styles to tour details page

--- client/src/components/TourInfo/TourInfo.tsx
+++ client/src/components/TourInfo/TourInfo.tsx
@@ -1,7 +1,60 @@
+import { FaRegHeart } from "react-icons/fa"
+import { FiShare2 } from "react-icons/fi"
+import { GrLocation } from "react-icons/gr"
+import styles from "./TourInfo.module.css"
+import { TiStarFullOutline } from "react-icons/ti"
+
 const TourInfo = () => {
   return (
-    <div>
-        
+    <div className={styles.body}>
+        <div style={{display: 'flex', paddingTop: '1rem', justifyContent: 'space-between'}}>
+            <div style={{display: 'flex', gap: '0.5rem'}}>
+                <GrLocation size={24} />
+                <span style={{display: 'flex', gap: '1rem'}}><p>Budapest, Hungary</p><p style={{color: '#EB565A'}}>View on map</p></span>
+            </div>
+            <div style={{display: 'flex', gap: '1rem'}}>
+                <FiShare2 size={22} />
+                <FaRegHeart size={22} />
+            </div>
+        </div>
+        <p style={{fontSize: '32px', fontWeight: 'bold'}}>Wonders of the West Coast & Kimberly</p>
+        <span style={{height: '1px', background: 'black', width: '100%', opacity: '0.2'}}></span>
+        <div className={styles.info}>
+            <div className={styles.infoBox}>
+                <span>From</span>
+                <p style={{color: '#dd464b'}}>$104</p>
+            </div>
+            <div>
+                <span>Duration</span>
+                <p>7 days</p>
+            </div>
+            <div>
+                <span>Max People</span>
+                <p>25</p>
+            </div>
+            <div>
+                <span>Min Age</span>
+                <p>12+</p>
+            </div>
+            <div>
+                <span>Tour Type</span>
+                <p>Adventure</p>
+            </div>
+            <div>
+                <span>Reviews</span>
+                <div style={{display: 'flex', gap: '0.2rem', alignItems: 'center'}}><TiStarFullOutline color="#dd464b" /><p style={{display: 'flex', gap: '0.2rem'}}>4.8<span style={{fontWeight: 'normal' }}>(15 reviews)</span></p></div>
+            </div>
+        </div>
+        <div>
+            <p className={styles.title}>Overview</p>
+            <p>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, offers an enchanting blend of cultures, sights, and experiences that captivate every traveler’s heart. As Turkey’s cultural and economic hub. Instabul seamlesly fuses its rich heritage with modernity, creating an unforgettable journey for visitors.</p>
+        </div>
+        <div>
+            <p className={styles.title}>Map</p>
+        </div>
+        <div>
+            <p className={styles.title}>Average Reviews</p>
+        </div>
     </div>
   )
 }
--- client/src/pages/TourDetails/TourDetails.tsx
+++ client/src/pages/TourDetails/TourDetails.tsx
@@ -1,6 +1,8 @@
 import BookTour from "../../components/BookTour/BookTour"
+import CarouselTour from "../../components/CarouselTour/CarouselTour"
 import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
+import TourInfo from "../../components/TourInfo/TourInfo"
 import styles from "./TourDetails.module.css"
 
 const TourDetails = () => {
@@ -10,11 +12,16 @@ const TourDetails = () => {
         <div className={styles.body}>
           <div className={styles.main}>
             <img src="https://3challenge-compass.s3.us-east-2.amazonaws.com/vernazza.jpg" alt="static-tour" style={{width: '100%'}} />
+            <TourInfo />
           </div>
           <div className={styles.aside}>
             <BookTour />
           </div>
         </div>
+        <div className={styles.bottom}>
+          <p style={{fontSize: '40px', fontWeight: 'bold', marginBottom: '3rem'}}>You may also like...</p>
+          <CarouselTour />
+        </div>     
       <Footer />
     </div>
   )
